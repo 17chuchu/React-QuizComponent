8e56aa067bde784b161d6890b24fee342bbcecbe
Unknown <17chuchu.guy@gmail.com>
2019-07-18 16:30:15 +0100

    Add babel

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import QuizQuestion from './QuizQuestion.js';
 
 let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component {
-  state = {
-    quiz_position: 1
-  };
+    state = {
+        quiz_position: 1
+    }
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    console.log(quizData);
-    return (
-      <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
-      </div>
-    );
-  }
+    render() {
+        console.log(quizData);
+        return (
+            <div>
+                <QuizQuestion
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                />
+            </div>
+        );
+    }
 }
 
 export default Quiz;
