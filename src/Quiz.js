87a8661108bbc4dd606a548397d064cfa8339285
Unknown <17chuchu.guy@gmail.com>
2019-07-18 16:10:21 +0100

    Prittier Formating code

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,33 +1,27 @@
-import QuizQuestion from './QuizQuestion.js'
-import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js';
+import React, { Component } from 'react';
 
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
 
+class Quiz extends React.Component {
+  state = {
+    quiz_position: 1,
+  };
 
+  constructor(props) {
+    super(props);
+  }
 
-class Quiz extends React.Component
-{
-
-    state = {
-        quiz_position: 1
-    }
-
-    constructor(props)
-    {
-        super(props)
-    }
-
-    render()
-    {
-        console.log(quizData)
-        return (
-            <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-            </div>
-        )
-    }
+  render() {
+    console.log(quizData);
+    return (
+      <div>
+        <QuizQuestion
+          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+        />
+      </div>
+    );
+  }
 }
-
-
 
 export default Quiz;
