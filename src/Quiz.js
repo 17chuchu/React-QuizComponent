eba5aafa55fddcc7b598926d338e30dc4d58b6c3
Unknown <17chuchu.guy@gmail.com>
2019-07-18 15:46:38 +0100

    Add set state

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,6 +7,10 @@ let quizData = require('./quiz_data.json')
 class Quiz extends React.Component
 {
 
+    state = {
+        quiz_position: 1
+    }
+
     constructor(props)
     {
         super(props)
@@ -18,7 +22,7 @@ class Quiz extends React.Component
         return (
             <div>
                 <div className='QuizQuestion'>
-                    {quizData['quiz_questions'][0]['instruction_text']}
+                    {quizData['quiz_questions'][this.state.quiz_position - 1]['instruction_text']}
                 </div>
             </div>
         )
